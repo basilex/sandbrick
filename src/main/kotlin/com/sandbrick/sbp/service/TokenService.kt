985559195721b209776fa6d5485b32f9b5f3fe35
Alexander Vasilenko <alexander.vasilenko@gmail.com>
2025-04-10 18:35:12 +0300

    AuthService transactional bug fixed

--- src/main/kotlin/com/sandbrick/sbp/service/TokenService.kt
+++ src/main/kotlin/com/sandbrick/sbp/service/TokenService.kt
@@ -4,6 +4,7 @@ import com.sandbrick.sbp.domain.Token
 import com.sandbrick.sbp.domain.User
 import com.sandbrick.sbp.domain.auth.TokenType
 import com.sandbrick.sbp.repository.TokenRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import java.time.Instant
 
@@ -21,23 +22,29 @@ class TokenService(
         return tokenRepository.save(token)
     }
 
+    @Transactional
     fun findByToken(token: String): Token? =
         tokenRepository.findByToken(token)
 
+    @Transactional
     fun deleteToken(token: String) =
         tokenRepository.deleteByToken(token)
 
+    @Transactional
     fun deleteTokensByUser(user: User) =
         tokenRepository.deleteAllByUser(user)
 
+    @Transactional
     fun deleteExpiredTokens(before: Instant = Instant.now()) =
         tokenRepository.deleteAllByExpiryDateBefore(before)
 
+    @Transactional
     fun isRefreshTokenValid(token: String): Boolean {
         val dbToken = tokenRepository.findByToken(token) ?: return false
         return dbToken.type == TokenType.REFRESH && dbToken.expiryDate.isAfter(Instant.now())
     }
 
+    @Transactional
     fun deleteAllUserTokens(user: User) {
         tokenRepository.deleteAllByUser(user)
     }
